2a95f5fb5a31ca2f2c22303e4558d4542d40ef6c
william-schlegel <william.schlegel@gmail.com>
2023-10-16 16:56:17 +0200

    new endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 // export const endpoint = 'https://ucheckin-back.herokuapp.com/api/graphql';
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `https://ucheckin-back.herokuapp.com/api/graphql`;
+export const prodEndpoint = `https://ucheckin-back-031e937084b4.herokuapp.com/api/graphql`;
 export const perPage = 25;
